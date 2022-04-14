a7b7471d0ff0c4a5f18813a78580f0b2c2ebe92d
SkozyVFX <skozy010@gmail.com>
2022-04-14 15:46:21 +0200

    3.6
    q

---
+++ interactions/verify.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('verifytekst')
+		.setDescription('Verify yourself on a server'),
+	async execute(interaction) {
+
+		const embed = new MessageEmbed()
+		.setAuthor({ name: 'Verificatie', iconURL: interaction.guild.iconURL()})
+        .setDescription("Klik op de knop om jezelf te verifiëren en toegang te krijgen tot alle kanalen. Door jezelf te verifiëren ga je automatisch akkoord met onze server voorwaarden.")
+
+		const row = new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+			.setCustomId('verify')
+			.setLabel('Verifiëren')
+			.setStyle('SUCCESS')
+		)
+
+
+		await interaction.reply({ embeds: [embed], components: [row]});
+	},
+};
